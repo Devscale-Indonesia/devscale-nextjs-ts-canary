16633c0135540e40b672f7e3d5877f553251ae11
rezarinaldi <rez4rinaldi@gmail.com>
2024-07-05 09:34:31 +0700

    upload template

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "@/styles/globals.css";
+
+const geistSans = localFont({
+  src: "../../public/fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../../public/fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Template Next.js TypeScript (Canary Version) | Devscale ID",
+  description:
+    "Template Project Next.js TypeScript (Canary Version) for Devscale ID",
+  icons: {
+    icon: "/icon.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/header";
+
+export default function Home() {
+  return (
+    <main className="text-xl font-semibold">
+      <Header />
+      <h2>Hello. Ini Template Project Next.js TypeScript versi Canary. 🔥</h2>
+    </main>
+  );
+}
